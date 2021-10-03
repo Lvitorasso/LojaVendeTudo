67e948cb7a58364f21a6973f2e1dfeb6569e1156
Lucas vitorasso <lucasvitorasso@hotmail.com>
2021-10-03 10:25:19 -0300

    login e cadastro

--- Angular/src/app/services/auth.service.ts
+++ Angular/src/app/services/auth.service.ts
@@ -1,30 +1,35 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, switchMap } from 'rxjs/operators';
-import { Observable  } from 'rxjs';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Observable, throwError  } from 'rxjs';
+import { BadRequestError } from './../Compartilhado/Erros/bad-request-error';
+import { AppError } from './../Compartilhado/Erros/app-error';
+import { NotFoundError } from './../Compartilhado/Erros/not-found-error';
+import { localStorageService } from './localStorageService';
 
 @Injectable()
 export class AuthService {
 
-  constructor( private http: HttpClient) {
+  private _url = 'https://localhost:44336';
+
+  constructor(private http: HttpClient, private localdb: localStorageService) {
   }
 
 
-  login(url: any, login: any) { 
-   return this.http.post( url+'/api/authenticate/login', JSON.stringify(login), httpOptions).pipe(map((response: any) => { 
+  login(login: any) { 
+   return this.http.post( this._url+'/api/authenticate/logar', JSON.stringify(login), httpOptions).pipe(map((response: any) => { 
 
-        let result = response.token;
-
-         if(result && result.token)
-         {
-        //   //localStorage.setItem('token', result.token);
-
-           return true;
-         }
+        if(response.token)
+        {
+          let result = response.token;
+          this.localdb.set('token', result);
+          this.localdb.set('usuario', response.usuario);
+          
+          return true;
+        }
 
         return false;
-      }));
+      }, catchError(this.handleError)));
   }
 
   logout() { 
@@ -35,16 +40,27 @@ export class AuthService {
   }
 
   
-  signUp(url: any, login: any) { 
-    return this.http.post( url+'/api/authenticate/signUp', JSON.stringify(login), httpOptions).pipe(map((response: any) => { 
-        return true;
+  cadastrar(pessoa: any) { 
+    return this.http.post( this._url+'/api/authenticate/cadastrar', JSON.stringify(pessoa), httpOptions).pipe(map((response: any) => { 
+        if(response.mensagemRetornoOK)
+          return true;
+        else
+          return false;
        }));
    }
- 
+   
+   private handleError(error: Response){
+    if(error.status == 404)
+      return throwError(new NotFoundError(error.json)); 
+    else if(error.status == 400)
+      return throwError(new BadRequestError(error.json));
+    else          
+      return throwError(new AppError(error.json));
+  }
 
 }
-
-
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
 }
+
+
