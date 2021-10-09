ef12fd3b3d5ce290f9e359f0136dd3b2455cc004
Lucas vitorasso <lucasvitorasso@hotmail.com>
2021-10-09 12:43:08 -0300

    Revert "teste heroku 2"
    This reverts commit 27b058749f8aaf7f80086c1967235593b3860212.

--- LojaVendeTudo.API/Startup.cs
+++ LojaVendeTudo.API/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -54,24 +53,11 @@ namespace LojaVendeTudo.API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LojaVendeTudo.API v1"));
             }
 
-            app.Use(async (context, next) =>
-            {
-                await next();
-
-                if(context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value))
-                {
-                    context.Request.Path = "/index.html";
-                    await next();
-                }
-
-            });
-
-            app.UseDefaultFiles();
             app.UseHttpsRedirection();
+
             app.UseRouting();
+
             app.UseAuthorization();
-            app.UseStaticFiles();
-            
 
             app.UseEndpoints(endpoints =>
             {
